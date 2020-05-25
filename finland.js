7eda36e0694363eeceb7aa21d87d5af936b7a5f9
Jonne Kaunisto <jonneka@gmail.com>
2020-05-25 15:50:39 -0700

    made density work

--- finland.js
+++ finland.js
@@ -13,53 +13,11 @@ var svg = d3.select("body")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//Define map projection
-/*
-var projection = d3.geoEquirectangular()
-    .translate([(width - 2200) / 2, (height + margin.top + margin.bottom + 5600) / 2])
-    .scale([2500]);
-*/
-/*
-scale = 700
-    //600, 1100, 200
-    //700, 1250, 150
-
-
-var projection = d3.geoMercator()
-    .translate([150, 1250])
-    .scale([scale]);;
-
-
-//Define path generator
-var path = d3.geoPath()
-    .projection(projection);
-
-
-
-
-console.log("here")
-    //Load in GeoJSON data
-    //"another_finland/gadm36_FIN_0.json"
-d3.json("low_res/gadm36_FIN_0.json").then(function(json) {
-    //Bind data and create one path per GeoJSON feature
-    console.log(json)
-
-    map = svg.append("g").attr("class", "map")
-    map.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", "blue")
-        .style("stroke", "black")
-        .style("stroke-width", "1px")
-
-});
-*/
+var color = d3.scaleThreshold()
+    .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
+    .range(d3.schemeOrRd[9]);
 
 scale = 1200
-
-
 var projection = d3.geoMercator()
     .translate([-100, 2100])
     .scale([scale]);;
@@ -76,6 +34,6 @@ d3.json("topo_with_data.json").then(function(topology) {
         .selectAll("path")
         .data(topojson.feature(topology, topology.objects.gadm36_FIN_2).features)
         .enter().append("path")
-        .attr("fill", "blue")
+        .attr("fill", function(d) { console.log(d.properties.pop_data["2019"]); return color(d.properties.pop_data["2019"]); })
         .attr("d", path);
 });
