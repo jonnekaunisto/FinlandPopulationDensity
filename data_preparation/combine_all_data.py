8aab9331dd9e41dde5d84bc17e3197e61d8d69af
Jonne Kaunisto <jonneka@gmail.com>
2020-05-26 13:43:25 -0700

    made new data

---
+++ data_preparation/combine_all_data.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import json
+
+data_file = "../raw_data_all_regions/population_finland.csv"
+geo_file = "../topo_without_data.json"
+area_file = "../raw_data_all_regions/area_finland.csv"
+
+kunta_trans = {
+    "Middle Karelia": "Keski-Karjala",
+    "Pielisen Karelia": "Pielisen Karjala",
+    "North Eastern Savonia": "Koillis-Savo",
+    "Upper Savonia": "Ylä-Savo",
+    "Eastern Lapland": "Itä-Lappi",
+    "Kemi-Tornio": "Kemi-Tornio",
+    "Northern Lapland": "Pohjois-Lappi",
+    "Tornio Valley": "Torniolaakso",
+    "Tunturi Lapland": "Tunturi-Lappi",
+    "Kehys-Kainuu": "Kehys-Kainuu",
+    "Koillismaa": "Koillismaa",
+    "Nivala-Haapajärvi": "Nivala-Haapajärvi",
+    "Oulunkaari": "Oulunkaari",
+    "Kotka-Hamina": "Kotka-Hamina",
+    "Western Saimaa": "MK11 Pohjois-Savo",
+    "Ekenäs": "Raasepori",
+    "South Eastern Middle": "MK06 Pirkanmaa",
+    "Åboland-Turunmaa": "Åboland-Turunmaa",
+    "Vakka-Suomi": "Vakka-Suomi",
+    "Jakobstadsregionen": "Jakobstadsregionen",
+    "Kyrönmaa": "Kyrönmaa",
+    "South Eastern Bothnia": "South Eastern Bothnia",
+    "North Western Pirkanm": "MK06 Pirkanmaa",
+    "South Eastern Pirkanm": "MK06 Pirkanmaa",
+    "South Western Pirkanm": "Lounais-Pirkanmaa",
+    "Southern Pirkanmaa": "Etelä-Pirkanmaa",
+    "Upper Pirkanmaa": "Ylä-Pirkanmaa",
+    "Northern Satakunta": "Pohjois-Satakunta",
+    "South Eastern Satakun": "MK04 Satakunta",
+    "Härmänmaa": "Kauhava",
+    "Järviseutu": "Järviseutu",
+    "Kuusiokunnat": "Kuusiokunnat",
+    "Northern Seinänaapuri": "Seinäjoki",
+    "South Eastern Bothnia": "MK14 Etelä-Pohjanmaa",
+    "Southern Seinänaapuri": "Seinäjoki"
+}
+
+
+land_trans = {
+    "Middle Karelia": "Pohjois-Karjala",
+    "Pielisen Karelia": "Pohjois-Karjala",
+    "North Eastern Savonia": "Pohjois-Savo",
+    "Upper Savonia": "Pohjois-Savo",
+    "Eastern Lapland": "Lappi",
+    "Kemi-Tornio": "Kemi",
+    "Northern Lapland": "Lappi",
+    "Tornio Valley": "Tornio",
+    "Tunturi Lapland": "Lappi",
+    "Kehys-Kainuu": "Kainuu",
+    "Koillismaa": "Lappi",
+    "Nivala-Haapajärvi": "Nivala",
+    "Oulunkaari": "Oulu",
+    "Kotka-Hamina": "Kotka",
+    "Western Saimaa": "Pohjois-Savo",
+    "Ekenäs": "Raasepori",
+    "South Eastern Middle": "Pirkkala",
+    "Åboland-Turunmaa": "Turku",
+    "Vakka-Suomi": "Varsinais-Suomi",
+    "Jakobstadsregionen": "Janakkala",
+    "Kyrönmaa": "Hämeenkyrö",
+    "South Eastern Bothnia": "Pirkanmaa",
+    "North Western Pirkanm": "Pirkanmaa",
+    "South Eastern Pirkanm": "Pirkanmaa",
+    "South Western Pirkanm": "Pirkanmaa",
+    "Southern Pirkanmaa": "Pirkanmaa",
+    "Upper Pirkanmaa": "Pirkanmaa",
+    "Northern Satakunta": "Satakunta",
+    "South Eastern Satakun": "Satakunta",
+    "Härmänmaa": "Päijät-Häme",
+    "Järviseutu": "Jämijärvi",
+    "Kuusiokunnat": "Kuusamo",
+    "Northern Seinänaapuri": "Seinäjoki",
+    "South Eastern Bothnia": "Pirkanmaa",
+    "Southern Seinänaapuri": "Seinäjoki"
+}
+
+def process_land_data():
+    land_data = pd.read_csv(area_file)
+
+
+    land_data_dict = {}
+    for row in land_data.itertuples():
+        land_data_dict[row[1]] = row[2]
+
+    return land_data_dict
+
+
+def process_pop_data():
+    pop_data = pd.read_csv(data_file)
+
+
+    pop_data_dict = {}
+    for row in pop_data.itertuples():
+        if row[1] in ["MANNER - SUOMI", "AHVENANMAA"]:
+            continue
+        region_data = {}
+
+        start_year = 2019
+        for i, elem in enumerate(row[2:]):
+            region_data[str(start_year+i)] = elem
+
+        pop_data_dict[row[1]] = region_data
+
+    return pop_data_dict
+
+def add_data_to_topo(name, pop_data_dict, land_data_dict):
+    with open(geo_file, "r") as f:
+        topo_json = json.loads(f.read())
+
+    target_list = topo_json["objects"]["gadm36_FIN_" + name]["geometries"]
+
+    #make sure all of them match
+    print("===============all that match =================")
+    for target in target_list:
+        region_name = target["properties"]["NAME_" + name]
+
+        region_name_trans = kunta_trans.get(region_name, region_name)
+        region_name_land_trans = land_trans.get(region_name, region_name)
+
+        if region_name_trans in pop_data_dict:
+            target["properties"]["pop_data"] = pop_data_dict[region_name_trans]
+        else:
+            print("\"{0}\": \"{0}\",".format(region_name))
+
+        if region_name_land_trans in land_data_dict:
+            target["properties"]["land_data"] = land_data_dict[region_name_land_trans]
+        else:
+            print("\"{0}\": \"{0}\",".format(region_name))
+
+
+    with open("topo_with_data.json", "w") as f:
+        f.write(json.dumps(topo_json))
+
+
+def main():
+
+    land_data_dict = process_land_data()
+   
+    pop_data_dict = process_pop_data()
+
+    add_data_to_topo("3", pop_data_dict, land_data_dict)
+
+    exit()
+
+    for region in land_data["Maakunta"]:
+        matched = False
+        for target in target_list:
+            if target["properties"]["NAME_2"] == region:
+                matched = True
+                break
+        if not matched:
+            print(region)
+
+    for target in target_list:
+        matched = False
+        for region in regions:
+            if target["properties"]["NAME_2"] == region:
+                matched = True
+        if not matched:
+            print(target["properties"]["NAME_2"])
+
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
--- finland.js
+++ finland.js
@@ -14,7 +14,7 @@ var svg = d3.select("body")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var color = d3.scaleThreshold()
-    .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
+    .domain([1, 5, 10, 15, 30, 60, 90, 100])
     .range(d3.schemeOrRd[9]);
 
 scale = 1200
@@ -32,8 +32,40 @@ d3.json("topo_with_data.json").then(function(topology) {
     console.log(topology)
     svg.append("g")
         .selectAll("path")
-        .data(topojson.feature(topology, topology.objects.gadm36_FIN_2).features)
+        .data(topojson.feature(topology, topology.objects.gadm36_FIN_3).features)
         .enter().append("path")
-        .attr("fill", function(d) { console.log(d.properties.pop_data["2019"]); return color(d.properties.pop_data["2019"]); })
+        .attr("fill", function(d) { return color(d.properties.pop_data["2019"] / d.properties.land_data); })
         .attr("d", path);
 });
+
+d3.select("#yearSlider").on("onchange", val => {
+    console.log(val)
+});
+/*
+var sliderSimple = d3
+    .sliderBottom()
+    .min(1990)
+    .max(2040)
+    .width(300)
+    .tickFormat(d3.format('.2%'))
+    .ticks(5)
+    .default(0.015)
+    .on('onchange', val => {
+        d3.select('p#value-simple').text(d3.format('.2%')(val));
+    });
+
+var gSimple = d3
+    .selectAll('#slider-simple')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(30,30)');
+
+
+console.log(gSimple)
+
+d3.select('p#value-simple').text(d3.format('.2%')(sliderSimple.value()));
+*/
+
+//d3.selectAll("#slider-simple").append
